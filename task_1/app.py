a0cdb163e9018e161bcc5d42bc9130123088b6bf
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2026-01-25 09:34:03 +0000

    task 1 workflow

---
+++ task_1/app.py
@@ -0,0 +1,14 @@
+"""Flask app using Redis hit counter"""
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """Return a simple greeting to confirm the app is running."""
+    return 'Hello, World! This is a Docker Challenge App.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
